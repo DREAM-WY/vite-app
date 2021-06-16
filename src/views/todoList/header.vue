ee70d34117ec8f1202a65e131afff845590f67a1
吴宇 <15070522056@163.com>
2021-06-16 23:01:33 +0800

    test

--- src/views/todoList/header.vue
+++ src/views/todoList/header.vue
@@ -1,17 +1,22 @@
 <template>
-	<input class="todo-input" placeholder="请输入" type="text" v-model="keyword" @keyup.enter="onClick" />
+	<input 
+	
+	class="todo-input" placeholder="请输入" type="text" v-model="keyword" @keyup.enter="onClick" />
 </template>
 <script lang="ts">
 import { defineComponent, inject, Ref, ref } from 'vue'
 import { Todo } from './index.d'
 export default defineComponent({
-	name: 'Header',
+	name: 'Header', 
+
 	emits: ['addTodoList'],
 	setup(props, { emit }) {
 		const keyword: Ref<string> = ref('')
 		const onClick = (): void => {
 			const todo: Todo = {
-				name: keyword.value,
+
+				name: 
+				keyword.value,
 				isChecked: false,
 			}
 			emit('addTodoList', todo)
